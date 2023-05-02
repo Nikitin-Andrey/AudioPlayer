4f93f0aad465bfb52bb1dd8d750a4ae125c9bed2
Nikitin-Andrey <81535987+Nikitin-Andrey@users.noreply.github.com>
2023-05-02 20:45:51 +0300

    Add files via upload

---
+++ Pleer_music/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pleer_music
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        string fileName;
+
+
+        private void open_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog()
+            {
+                Multiselect = false,
+                DefaultExt = ".mp3"
+            };
+            bool? dialogOk = fileDialog.ShowDialog();
+            if (dialogOk == true)
+            {
+                fileName = fileDialog.FileName;
+                FileNameTextBox.Text = fileDialog.SafeFileName;
+                mediaPlayer.Open(new Uri(fileName));
+            }
+        }
+
+        private void play_Button_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void pause_Button_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void stop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+    }
+}
